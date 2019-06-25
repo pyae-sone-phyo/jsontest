b72d814a07b7e66165a2b95e0d8a1e3c27d03bce
Pyae Sone Phyo <pyaesone@zwenex.com>
2019-06-25 13:21:24 +0630

    Add home page in ui folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jsontest/ui/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,10 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Json Test',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
   }
 }
